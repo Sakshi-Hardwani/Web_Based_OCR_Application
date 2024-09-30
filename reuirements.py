c40bfa4f22109b73cbb72a7525a8d1bf03bf8f8d
Sakshi Hardwani <117386798+Sakshi-Hardwani@users.noreply.github.com>
2024-09-30 21:02:44 +0530

    Update reuirements.py

--- reuirements.py
+++ reuirements.py
@@ -1,7 +1,4 @@
-pip install --upgrade byaldi
-pip install flash-attn
-pip install -q pdf2image git+https://github.com/huggingface/transformers.git qwen-vl-utils flash-attn
 pip install pytesseract
 pip install numpy opencv-python
 pip install Pillow
-pip install streamlit pytesseract pillow
+pip install streamlit
